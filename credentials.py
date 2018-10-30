29153246586033ff53c95d96fbc32eea57a9dacf
JoshuaSturm <22535419+JoshuaSturm@users.noreply.github.com>
2018-10-30 17:50:29 -0400

    credential fix

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# This file contains login credentials for kaggle
+
+# email = 'iamfleurdelys@gmail.com'
+# password = 'tempproject'
+
+login = {
+    'email' : 'iamfleurdelys@gmail.com',
+    'password' : 'tempproject'
+}
